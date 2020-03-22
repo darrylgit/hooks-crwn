3a5f339a024747ecf296e8b36faa5d756379ff3a
darrylgit <darrylsmithup@gmail.com>
2020-03-22 16:16:58 -0400

    Add .env to gitignore

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
